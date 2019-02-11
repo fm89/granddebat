b163ca96e6cd5e76b55c64dd1e9eef308dd5dd11
FM <FM>
2019-02-11 18:46:53 +0100

    Pour pouvoir mettre en cache

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function show(Request $request)
+    {
+        return $request->user();
+    }
+}
